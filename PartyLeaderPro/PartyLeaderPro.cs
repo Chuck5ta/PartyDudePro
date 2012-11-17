a1f94ef84f56a81bbfb95d4e22e9d25c5db8551d
Charles Andrew Edwards <charles.a.edwards@btinternet.com>
2012-11-17 16:25:09 +0000

    Version 2.0.6 (had to go back to earlier version)
    2.0.7 caused crashes, therefore I have gone back to 2.0.6
    
    Leader leaves game and party.

--- PartyLeaderPro/PartyLeaderPro.cs
+++ PartyLeaderPro/PartyLeaderPro.cs
@@ -47,8 +47,8 @@ using Zeta.Internals.SNO;
 	
 	Author: ChuckyEgg (CIGGARC Developer)
 	Support: CIGGARC team, et al, especially Tesslarc ;)
-	Date: 17th of November, 2012
-	Verion: 2.0.7
+	Date: 16th of November, 2012
+	Verion: 2.0.6
 	
  */
 namespace PartyLeaderPro
@@ -162,7 +162,7 @@ namespace PartyLeaderPro
 
         public Version Version
         {
-            get { return new Version(2, 0, 7); }
+            get { return new Version(2, 0, 5); }
         }
 
         /// <summary> Executes the shutdown action. This is called when the bot is shutting down. (Not when Stop() is called) </summary>
